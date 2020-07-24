248722ed0246b1e1a9e87b1fa97551ff74af94f9
Mani181297 <manideep.r@ibhubs.co>
2020-07-24 14:24:23 +0530

    store testing files extension changed

--- src/Admin/stores/TabsStore/TabsStore.test.ts
+++ src/Admin/stores/TabsStore/TabsStore.test.ts
@@ -3,12 +3,12 @@ import TabsStore from '.';
 const tabsStore = new TabsStore();
 
 describe('tabs store test cases', () => {
-  it('should test initial tab status', async () => {
+  it('should test initial tab status', () => {
     expect(tabsStore.tabStatus).toBe('Resources');
   });
 
-  it('should test tab status after clicking users tab', async () => {
-    await tabsStore.updateTabStatus('Users');
+  it('should test tab status after clicking users tab', () => {
+    tabsStore.updateTabStatus('Users');
     expect(tabsStore.tabStatus).toBe('Users');
   });
 });
